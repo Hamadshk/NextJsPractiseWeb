cffb82a35a883187574250f9d8c264b514ca7248
Hamadshk <gsky925@gmail.com>
2024-08-14 19:54:38 +0500

    first commit

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Inside the Customer</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoice/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Inside the invoice</div>
+  )
+}
+
+export default page
